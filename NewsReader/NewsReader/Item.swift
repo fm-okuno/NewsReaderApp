ccee25fc57f1c9e9b70f7da4b2c4126803c95b62
gohta <okuno@freemake.co.jp>
2020-04-13 10:45:07 +0900

    Info.plist 　・App Transport Securityの設定をhttpから始まるサイトにもアクセスできる様に変更
    Item.swift
    　・ファイルの追加
    　・記事のクラスを作成
    
    ListViewController.swift
    　・セルを作成して内容を表示するメソッドを宣言
    　・RSSデータを解析し、要素名がitemのデータを取得
    　・要素名を基に内容を取り出し、内容を振り分けて格納
    　・振り分けた内容をreloadDataメソッドを使い、テーブルビューを書き換える事で表示する。（セルを作成して内容を表示するメソッドを実行）

---
+++ NewsReader/NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by 開発アカウント on 2020/04/10.
+//  Copyright © 2020 開発アカウント. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var link = ""
+}
